5438aed49ee70463a3ab4d397e3c32aecfd200ba
Maksimdon1 <maksimkozyrev056@gmail.com>
2023-08-15 23:47:17 +0300

    1

--- index.js
+++ index.js
@@ -1,64 +1,7 @@
-
-
-
-const express = require('express');
-
-
-const filepath = './users.json'
+const express = require('express')
 const app = express()
-app.get('/name', (req, res) => {
+app.all('/', (req, res) => {
     console.log("Just got a request!")
-    
-    console.log(req.headers['x-forwarded-for'] )
-    res.send('ip =   '+ req.headers['x-forwarded-for'])
+    res.send('Yo!')
 })
-
-  
-
- 
-
-app.get("/user/light", function(req, res) {
-    
-    let data = fs.readFileSync("./users.json", "utf8");
-    let js = JSON.parse(data);
-    res.send(js)
-    
-
-});
-app.put("/server/light", function(req, res) {
-    console.log( req.body )
-
-
-    if(     req.body.id && req.body.value   ){
-        const {id, value} = req.body
-        console.log(req.socket.remoteAddress)
-
- 
-
-        let data = fs.readFileSync("./users.json", "utf8");
-        let js = JSON.parse(data);
-        let device = js[id][0]
-        const now = new Date
-        get_time = now.toLocaleString()
-        console.log(now.toLocaleString())
-        device.time_updated = get_time
-        device.value = value
-        console.log(value)
-        js[id][0] = device
-        fs.writeFileSync("./users.json", JSON.stringify(js));
-        res.send('123')
-
-    }
-    else{
-        res.status(400).send("Failed to change")
-    }
-
-    
-});
-
-
-
-
-
-
 app.listen(process.env.PORT || 3000)
